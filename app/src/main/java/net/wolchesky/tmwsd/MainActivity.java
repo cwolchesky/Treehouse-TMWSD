a3933318028c524f7fc1d249550deca49b642962
Chris <chris@wolchesky.net>
2014-09-27 12:27:36 -0400

    Starting to query inbox for new messages

--- app/src/main/java/net/wolchesky/tmwsd/MainActivity.java
+++ app/src/main/java/net/wolchesky/tmwsd/MainActivity.java
@@ -16,9 +16,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.io.File;
@@ -26,6 +31,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 
