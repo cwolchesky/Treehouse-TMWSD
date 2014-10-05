ec79f86afa690de77d6e8dc1b0bbeb8befca99dc
Chris <chris@wolchesky.net>
2014-10-05 09:57:48 -0400

    Display image messages in a new activity using Picasso

---
+++ app/src/main/java/net/wolchesky/tmwsd/ViewImageActivity.java
@@ -0,0 +1,28 @@
+package net.wolchesky.tmwsd;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class ViewImageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_image);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Uri imageUri = getIntent().getData();
+
+        Picasso.with(this).load(imageUri).into(imageView);
+    }
+
+
+
+}
