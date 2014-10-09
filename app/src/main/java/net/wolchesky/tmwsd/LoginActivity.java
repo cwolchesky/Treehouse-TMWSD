c44306834e8fea9edda152a7eb338584416b53e3
Chris <chris@wolchesky.net>
2014-10-09 13:15:10 -0400

    Minor change to LoginActivity and SignUpActivity (Hid action bar).  Added resources for new iconography and images to VCS.

--- app/src/main/java/net/wolchesky/tmwsd/LoginActivity.java
+++ app/src/main/java/net/wolchesky/tmwsd/LoginActivity.java
@@ -1,5 +1,6 @@
 package net.wolchesky.tmwsd;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -26,6 +27,10 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_login);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+
 
         mSignupTextView = (TextView) findViewById(R.id.signupText);
 
